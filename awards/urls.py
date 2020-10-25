4cf971f037b58a1b4475d3d40ac8f7586efedfb9
kelvin-daniel <kaymutor@gmail.com>
2020-10-25 17:03:18 +0300

    create index view

--- awards/urls.py
+++ awards/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('awords.urls')),
-    path('accounts/',include('registration.backends.simple.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('logout/',views.logout,{'next_page':'/'}),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- awords/urls.py
+++ awords/urls.py
@@ -5,21 +5,17 @@ from django.conf.urls.static import static
 from django.conf import settings
 from rest_framework.authtoken.views import obtain_auth_token
 
-
 #url urlpatterns
 
 urlpatterns=[
-    url(r'^$',views.index, name='index'),
-    url(r'project/post/$',views.post,name='post'),
-    url(r'^user/profile/$',views.profile,name='profile'),
-    url(r'^project/(\d+)/',views.project_detail,name='details'),
-    url(r'^search/projects/results/$',views.search,name="search"),
+   path('',views.index, name='index'),
+   path('project/post/',views.post,name='post'),
+   path('user/profile/',views.profile,name='profile'),
+   path('project/(\d+)/',views.project_detail,name='details'),
+   path('search/projects/results/',views.search,name="search"),
     # url(r'^ajax/review/(\d+)$',views.ajaxRequest,name='review'),
-    url(r'^api/projects/$',views.ProjectList.as_view()),
-    url(r'^api/profile/$',views.ProfileList.as_view()),
-    url(r'^token/', obtain_auth_token),
-    url(r'^developer/api/$',views.apiView,name='api'),
+    path('api/projects/',views.ProjectList.as_view()),
+    path('api/profile/',views.ProfileList.as_view()),
+    path('token/', obtain_auth_token),
+    path('developer/api/',views.apiView,name='api'),
 ]
-
-if settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
